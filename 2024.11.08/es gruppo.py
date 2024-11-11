6c32e88f9a18df9623fe65b6da97011abd83d348
Alessio Giuseppe Ferraioli <alessioferraioli@hotmail.it>
2024-11-11 10:38:46 +0100

    Update es gruppo.py

---
+++ 2024.11.08/es gruppo.py
@@ -0,0 +1,36 @@
+'''
+
+traccia esercizio di gruppo
+
+fare un gestionale ripetibile (quindi accedibile da piu utenti)
+tutto deve essereo oggetto (no oggetto fuori dalle classe
+
+
+tre cose fa:
+
+1 login , registrazione, 
+    menu: ciao! digita 1 login, 2 registrati, 3 esci
+
+2 funzionalita che tiene conto di chi sei e abbina il punteggio, esposto a fianco a tuo nome
+    questo punteggio definisce quante funzionalita vede l'utente
+    ovvero all'inzio ogni utente ha zero punti e può svolgere solo la funzione 1: ovvero rispondere a una somma
+        se la risposta è corretta, gli diamo dei punti, se sbaglia glieli togliamo i punti
+
+    in base a quanti punti ha accede a piu funzioni
+        se ha da 0 a 5 fa la somma di due addendi
+        se ha da 5 a 15 fa la somma di tre addendi
+         ecc ecc per un massimo di 10 livelli di difficolta
+
+3 vorrei la classifica di tutti gli utenti che hanno giocato e deve salvare la classifica come file quando si chiude
+
+
+
+
+
+sistema di registrazione login che difende un gestionale funzionale evolutivo
+perhcè ha un sistema di gamification
+ogni volta che una azione ritorna un ture (vittoria) aumenta punteggio dell'utente di x
+piu punteggio sale piu diventano difficili le sfide per l'utente
++
+sistema classifica che deve essere stampata a file
+'''
