567ba7c67eb971aeda94be602552e56987901f6d
Alessio Giuseppe Ferraioli <alessioferraioli@hotmail.it>
2024-11-12 18:33:37 +0100

    Update es sql.sql

--- 2024.11.12/es sql.sql
+++ 2024.11.12/es sql.sql
@@ -17,3 +17,28 @@ Scrivete una query SQL che aggiorni il prezzo di tutti i prodotti nella tabella"
 
 UPDATE 'products'
 SET 'buyPrice' = 'buyPrice'*1.1;
+
+#Scrivete una query SQL che elimini tutti gli ordini nella tabella "
+#orders" con hanno lo stato "Cancelled".
+
+DELETE FROM orderdetails
+WHERE orderNumber IN ( 
+    SELECT orderNumber 
+    FROM orders 
+    WHERE status = "Cancelled" );
+
+DELETE FROM orders
+WHERE status = "Cancelled";    
+
+#Scrivete una query SQL che restituisca tutti gli utenti dalla tabella"customers"
+# il cui nome inizia con la S e vivono in California.
+
+SELECT * 
+FROM customers
+WHERE state = "CA" 
+    and customerName like "S%";
+
+
+#Si vogliono recuperare dal database "world" la lingua e la nazione di ogni città.
+select *
+from (JOIN country
