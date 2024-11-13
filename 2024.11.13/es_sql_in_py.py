32cc7bec00aa0363d304d63fdd33268b691f7139
Alessio Giuseppe Ferraioli <alessioferraioli@hotmail.it>
2024-11-13 14:52:41 +0100

    Update es_sql_in_py.py

--- 2024.11.13/es_sql_in_py.py
+++ 2024.11.13/es_sql_in_py.py
@@ -1,21 +1,45 @@
 import mysql.connector
 
-
+'''
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="root"
 )
 
+
+
 #print(mydb)
 
+#mycursor = mydb.cursor()
+# crea database
+#query = "CREATE DATABASE PYTHONMYSQL"
+# mycursor.execute(query)
+
+# show databases
+query = "show databases"
+mycursor.execute(query)
+for x in mycursor:
+    print(x)
+
+'''
+
+# fare il connector con un solo database (invece che tutti quello sull'host)
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database = "pythonmysql"
+)
+# creo un cursore per questo connector "mydb" che contiene solo il database "pythonmysql"
 mycursor = mydb.cursor()
 
-#query = "CREATE DATABASE PYTHONMYSQL"
-
-query = "show databases"
-
+#creo una query per creare tabella
+query = "CREATE TABLE utenti (ID int auto_increment primary key, nome VARCHAR(50), indirizzo VARCHAR(50))"
 mycursor.execute(query)
 
+# mostrare le tables
+query = "SHOW TABLES"
+mycursor.execute(query)
 for x in mycursor:
     print(x)
