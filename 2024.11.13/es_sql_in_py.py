cccd4dfd0f237d3d37f2ef38b747e3346ea6cfaf
Alessio Giuseppe Ferraioli <alessioferraioli@hotmail.it>
2024-11-13 14:27:11 +0100

    Update es_sql_in_py.py

--- 2024.11.13/es_sql_in_py.py
+++ 2024.11.13/es_sql_in_py.py
@@ -1,9 +1,21 @@
 import mysql.connector
 
+
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="root"
 )
 
-print(mydb)
+#print(mydb)
+
+mycursor = mydb.cursor()
+
+#query = "CREATE DATABASE PYTHONMYSQL"
+
+query = "show databases"
+
+mycursor.execute(query)
+
+for x in mycursor:
+    print(x)
